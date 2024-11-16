0dbd159ce3c90dd83774e645705e7751c1dacf33
telugu-boy <42505125+telugu-boy@users.noreply.github.com>
2024-11-15 20:11:33 -0700

    add websocket and openbci wrappeur

---
+++ src-eeg-emissary/openbci.py
@@ -0,0 +1,20 @@
+from brainflow import BrainFlowInputParams, BoardShim, BoardIds, BrainFlowPresets
+
+class CytonDaisy:
+    def __init__(self, serial_port = "COM17"):
+        self.params = BrainFlowInputParams()
+        self.params.serial_port = serial_port
+        self.board_preset = BrainFlowPresets.DEFAULT_PRESET
+        self.board: BoardShim = BoardShim(BoardIds.CYTON_DAISY_BOARD, self.params)
+
+    def __enter__(self):
+        self.board.prepare_session()
+        self.board.start_stream()
+        return self
+
+    def get_data(self):
+        return self.board.get_board_data(num_samples=None, preset=self.board_preset)
+    
+    def __exit__(self,  exception_type, exception_value, exception_traceback):
+        self.board.stop_stream()
+        self.board.release_session()
---
+++ src-eeg-emissary/ws.py
@@ -0,0 +1,61 @@
+from websockets.asyncio.server import serve, ServerConnection
+from openbci import CytonDaisy
+import asyncio
+import json
+from dataclasses import dataclass
+from typing import *
+
+@dataclass
+class RWs_InitComms:
+    code = "INIT"
+    ivl: int # interval in ms to receive data
+
+# List of Event Type, Onset, Duration
+TimestampData = List[Tuple[int, float, float]]
+
+@dataclass
+class RWs_Timestamps:
+    code = "TIMESTAMPS"
+    data: TimestampData
+
+EEGData = List[float]
+
+@dataclass
+class EWs_EmitLatest:
+    code = "EMISSION"
+    chs: int # channels
+    n: int # number of samples
+    data: EEGData # chs x n array of float
+
+RecvWSMsgs = Union[RWs_InitComms, RWs_Timestamps]
+EmitWSMsgs = Union[EWs_EmitLatest]
+
+def interpret_msg(msg) -> RecvWSMsgs:
+    msg_dict: RecvWSMsgs = json.loads(msg)
+    res: RecvWSMsgs = None
+
+    if msg_dict['code'] == "INIT":
+        ivl = int(msg_dict['ivl'])
+        res = RWs_InitComms(ivl=ivl)
+    elif msg_dict['code'] == "TIMESTAMPS":
+        data: TimestampData = map(lambda ts: (int(ts[0]), float(ts[1]), float(ts[2])), msg_dict['data'])
+        res = RWs_Timestamps(data=list(data))
+
+    return res
+
+async def handler(ws: ServerConnection):
+    try:
+        while True:
+            message = interpret_msg(await ws.recv())
+    except ws.ConnectionClosed:
+        pass
+
+async def start_srv():
+    async with serve(handler, "", 8001):
+        await asyncio.get_running_loop().create_future()
+
+if __name__ == "__main__":
+    #asyncio.run(start_srv())
+    with CytonDaisy("COM17") as board:
+        print(board.board.is_prepared())
+        print("dssdf", str(board.get_data()))
