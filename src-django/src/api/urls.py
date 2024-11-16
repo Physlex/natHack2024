25b552e89ec62d7236a8cf3c1c1c7e9e859bcd4f
Alex <al.willet02@gmail.com>
2024-11-16 14:08:39 -0700

    fixed django errros

--- src-django/src/api/urls.py
+++ src-django/src/api/urls.py
@@ -9,18 +9,18 @@ from . import views
 urlpatterns = [
     # EEG
     path(
-        "/eeg/<int:eeg_id>",
+        "eeg/<int:eeg_id>",
         views.EEGModelReadView.as_view(),
         name="EEG Download Endpoint",
     ),
     path(
-        "/eeg",
+        "eeg",
         views.EEGModelCreateView.as_view(),
         name="EEG Creation Endpoint"
     ),
     # DIFFUSER
     path(
-        "/diffuser/generate",
+        "diffuser/generate",
         views.DiffuserGenerateVideoView.as_view(),
         name="ML Endpoint",
     ),
