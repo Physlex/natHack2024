6f9822efd6fd47b0ea53128ba5546e3b35bc5366
Max 🤪 <maksimus@ualberta.ca>
2024-11-17 00:14:02 -0700

    Moved buttons around a bit, going to recenter video, need to link video to black box

--- src-vite/src/pages/diffuser/Diffuser.tsx
+++ src-vite/src/pages/diffuser/Diffuser.tsx
@@ -90,11 +90,12 @@ export default function Diffuser(): JSX.Element {
         }
     }
     // Enable/disabling download button when it is available
-    const [isDownloadEnabled, setDownloadEnabled] = useState(false);
+    //const [isDownloadEnabled, setDownloadEnabled] = useState(false);
+    const [isDownloadEnabled] = useState(false);
     // Temporary code so I do not forget how this works
-    const handleEnableDownload = () => {
-      setDownloadEnabled(true); // Enable the button
-    };
+    // const handleEnableDownload = () => {
+    //   setDownloadEnabled(true); // Enable the button
+    // };
 
     return (
         <Grid container sx={{ height: '100vh' }}>
@@ -132,15 +133,6 @@ export default function Diffuser(): JSX.Element {
               margin: 3,
             }}
           >
-            <Button
-                className="generate-diffusion"
-                variant="outlined"
-                size="large"
-                onClick={handleButtonClick}
-                >
-                
-                Generate!
-            </Button>
             <Box
                 sx={{
                     width: 800,
@@ -151,15 +143,34 @@ export default function Diffuser(): JSX.Element {
                 }}
             >
             </Box>
-            <Button
-                className="donwnload-diffusion"
-                variant="outlined"
-                size="large"
-                endIcon={<DownloadIcon />}
-                disabled
-                >
-                Download
-            </Button>
+            {/* Grid to put buttons left to right instead */}
+            <Box
+                sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center", // Align buttons vertically in the center
+                width: 800, // Ensure the box spans the full width
+                }}
+            >
+                <Button
+                    className="generate-diffusion"
+                    variant="outlined"
+                    size="large"
+                    onClick={handleButtonClick}
+                    >
+                    
+                    Generate!
+                </Button>
+                <Button
+                    className="donwnload-diffusion"
+                    variant="outlined"
+                    size="large"
+                    endIcon={<DownloadIcon />}
+                    disabled={!isDownloadEnabled} // Button is disabled unless state is true
+                    >
+                    Download
+                </Button>
+            </Box>
           </Paper>
         </Grid>
       </Grid>
