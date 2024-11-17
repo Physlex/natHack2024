20998581fd1322c8f1ecf1165c1a3fb1edc529e3
Alex <al.willet02@gmail.com>
2024-11-16 20:38:42 -0700

    ran black

--- src-django/src/api/urls.py
+++ src-django/src/api/urls.py
@@ -13,11 +13,7 @@ urlpatterns = [
         views.EEGModelReadView.as_view(),
         name="EEG Read Endpoint",
     ),
-    path(
-        "eeg",
-        views.EEGModelCreateView.as_view(),
-        name="EEG Create Endpoint"
-    ),
+    path("eeg", views.EEGModelCreateView.as_view(), name="EEG Create Endpoint"),
     # DIFFUSER
     path(
         "diffuser/generate",
