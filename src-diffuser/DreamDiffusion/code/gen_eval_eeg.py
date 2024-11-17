9878f1d2ab35c8d3aa35a611c31e9844d2fc88d3
hrithick <hrithickchak@gmail.com>
2024-11-17 08:52:26 -0700

    fixed api.py

--- src-diffuser/DreamDiffusion/code/gen_eval_eeg.py
+++ src-diffuser/DreamDiffusion/code/gen_eval_eeg.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     #output_path = os.path.join(config.root_path, 'results', 'eval',  
     #               '%s'%(datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")))
-    output_path = os.path.join('....../generated')
+    output_path = os.path.join('../../../generated')
 
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
