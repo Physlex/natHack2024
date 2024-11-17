2ab98b813d34116e731154a63395df009901d5cc
Alex <al.willet02@gmail.com>
2024-11-16 20:38:42 -0700

    ran black

--- src-django/src/api/views.py
+++ src-django/src/api/views.py
@@ -28,24 +28,25 @@ class EEGModelReadView(APIView):
 class EEGModelCreateView(APIView):
 
     def post(self, request: Request) -> Response:
-        timeseries = request.data['timeseries']
-        timestamps = request.data['timestamps']
-        if (timeseries is None or timestamps is None):
+        timeseries = request.data["timeseries"]
+        timestamps = request.data["timestamps"]
+        if timeseries is None or timestamps is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
         eeg_model = models.EEGModel.objects.create()
         samples = []
         for channel_idx, channel in enumerate(timeseries):
             eeg_channel = models.EEGChannel.objects.create(
-                model=eeg_model,
-                channel=channel_idx
+                model=eeg_model, channel=channel_idx
             )
             for sample_idx, sample in enumerate(channel):
-                samples.append(models.EEGSample.objects.create(
-                    data=sample,
-                    timestamp=timestamps[sample_idx],
-                    channel=eeg_channel,
-                ))
+                samples.append(
+                    models.EEGSample.objects.create(
+                        data=sample,
+                        timestamp=timestamps[sample_idx],
+                        channel=eeg_channel,
+                    )
+                )
 
         models.EEGSample.objects.bulk_create(samples)
         return Response(status=status.HTTP_201_CREATED)
